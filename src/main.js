4146490d9ee42b23b0da3c6bf9538b611d5e8e30
JunderKing <junderking@163.com>
2024-07-17 08:06:00 +0800

    build

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import {
 	createSSRApp
 } from "vue";
 import App from "./App.vue";
+import eruda from 'eruda'
+
+eruda.init()
+
 export function createApp() {
 	const app = createSSRApp(App);
 	return {
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,8 @@
 <template>
   <view class="content">
+    <div style="width: 90vw;">USER: {{ initData.user }}</div>
     <div id="ton-connect"></div>
-    <!-- <button @click="connectWallet">CONNECT</button> -->
+    <button @click="sendTx">PAY TON</button>
   </view>
 </template>
 
@@ -11,19 +12,37 @@ import {TonConnectUI} from '@tonconnect/ui';
 export default {
   data() {
     return {
-      tonConnectUI: null
+      tonConnectUI: null,
+      webApp: null,
+      initData: {},
     }
+  },
+  onLoad() {
+    this.webApp = window.window.Telegram.WebApp 
+    console.log('webapp', window.window.Telegram.WebApp)
+    this.initData = this.webApp.initDataUnsafe
   },
   onReady() {
     this.tonConnectUI = new TonConnectUI({
-      manifestUrl: 'https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json',
+      // manifestUrl: 'https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json',
+      manifestUrl: 'https://tma.charsoft.tech/static/tonconnect-manifest.json',
       buttonRootId: 'ton-connect'
     });
   },
   methods: {
     async connectWallet() {
       const connectedWallet = await this.tonConnectUI.connectWallet();
-      console.log(connectedWallet)
+      console.log('wallet', connectedWallet)
+    },
+    async sendTx() {
+      const transaction = {
+        messages: [{
+          address: "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // 目标地址
+          amount: "10000000" //以nanotons计的 0.1 Toncoin
+        }]
+      }
+      const result = await this.tonConnectUI.sendTransaction(transaction)
+      console.log({result})
     }
   },
 }
@@ -31,6 +50,8 @@ export default {
 
 <style>
 .content {
+  width: 100vw;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
